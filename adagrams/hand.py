40a9a78c131ad1b06b0ca582ef9eae02d88a8d15
Becca Elenzil <becca@adadevelopersacademy.org>
2021-09-24 11:33:49 -0700

    Create hand class and populate letter bank with draw_letters method

---
+++ adagrams/hand.py
@@ -0,0 +1,30 @@
+import copy
+import random
+
+class Hand:
+    def __init__(self, letter_pool):
+        self.letter_bank = self.draw_letters(letter_pool)
+
+    def draw_letters(self, letter_pool):
+        letter_pool_copy = copy.deepcopy(letter_pool)
+        letters = []
+
+        while len(letters) < 10:
+            letter = random.choice(list(letter_pool_copy.keys()))
+            if letter_pool_copy[letter] > 0:
+                letters.append(letter)
+                letter_pool_copy[letter] -= 1
+
+        return letters
+
+    def uses_available_letters(word, letter_bank):
+        letter_bank_copy = copy.deepcopy(letter_bank)
+
+        for letter in word:
+            if letter in letter_bank_copy:
+                letter_bank_copy.remove(letter)
+            else:
+                return False
+        
+        return True
+
